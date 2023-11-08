13491277de65566a8649dc6a2ce9bb88e6604866
AaronCordero-VDM <aaronciempo@gmail.com>
2023-11-08 11:28:06 +0100

    08/11

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const nav = document.querySelectorAll(".nav");
 menuBtn.addEventListener("click", () => {
   document.body.classList.toggle("open"); 
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const sliders = document.querySelectorAll("input[type=radio]");
+  let currentIndex = 0;
+
+  function nextSlide() {
+      sliders[currentIndex].checked = false;
+      currentIndex = (currentIndex + 1) % sliders.length;
+      sliders[currentIndex].checked = true;
+  }
+
+  setInterval(nextSlide, 4000); // Cambia de imagen cada 3 segundos
+});
